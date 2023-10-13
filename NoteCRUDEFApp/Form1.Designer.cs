d387c5e737b4c2bf26e53ad43fc9acc71be04c7e
Aizhan <aizhankarasartova@gmail.com>
2023-10-13 22:02:28 +0600

    Добавьте файлы проекта.

---
+++ NoteCRUDEFApp/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace NoteCRUDEFApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.dgvNotes = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(65, 93);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(65, 150);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Note";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtTitle.Location = new System.Drawing.Point(127, 90);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(255, 23);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // txtNote
+            // 
+            this.txtNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtNote.Location = new System.Drawing.Point(127, 150);
+            this.txtNote.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtNote.Multiline = true;
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(255, 208);
+            this.txtNote.TabIndex = 3;
+            // 
+            // dgvNotes
+            // 
+            this.dgvNotes.AllowUserToAddRows = false;
+            this.dgvNotes.AllowUserToDeleteRows = false;
+            this.dgvNotes.AllowUserToResizeColumns = false;
+            this.dgvNotes.AllowUserToResizeRows = false;
+            this.dgvNotes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvNotes.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dgvNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNotes.ColumnHeadersVisible = false;
+            this.dgvNotes.Location = new System.Drawing.Point(435, 90);
+            this.dgvNotes.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.dgvNotes.Name = "dgvNotes";
+            this.dgvNotes.ReadOnly = true;
+            this.dgvNotes.RowHeadersVisible = false;
+            this.dgvNotes.RowTemplate.Height = 40;
+            this.dgvNotes.Size = new System.Drawing.Size(264, 307);
+            this.dgvNotes.TabIndex = 4;
+            this.dgvNotes.DoubleClick += new System.EventHandler(this.dgvNotes_DoubleClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(127, 374);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(74, 32);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Silver;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCancel.Location = new System.Drawing.Point(307, 374);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(74, 32);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.LightCoral;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDelete.Location = new System.Drawing.Point(217, 374);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(74, 32);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(301, 32);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 32);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Easy Note";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(740, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dgvNotes);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.DataGridView dgvNotes;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ NoteCRUDEFApp/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace NoteCRUDEFApp
+{
+    public partial class Form1 : Form
+    {
+        NotesTab model = new NotesTab();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            PopulateDataGridView();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void Clear()
+        {
+            txtTitle.Clear();
+            txtNote.Clear();
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            model.Id = 0;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            model.Title = txtTitle.Text.Trim();
+            model.Note = txtNote.Text.Trim();
+            using (DBEntities db = new DBEntities())
+            {
+                if(model.Id == 0) //Insert
+                db.NotesTab.Add(model);
+                else //Update
+                    db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            Clear();
+            PopulateDataGridView();
+            MessageBox.Show("Submitted Successfully");
+        }
+
+        void PopulateDataGridView()
+        {
+            using(DBEntities db = new DBEntities())
+            {
+                dgvNotes.DataSource = db.NotesTab.ToList<NotesTab>();
+                dgvNotes.Columns["Id"].Visible = false;
+                dgvNotes.Columns["Note"].Visible = false;
+            }
+        }
+
+        private void dgvNotes_DoubleClick(object sender, EventArgs e)
+        {
+            if(dgvNotes.CurrentRow.Index != -1)
+            {
+                model.Id = Convert.ToInt32(dgvNotes.CurrentRow.Cells["Id"].Value);
+                using(DBEntities db = new DBEntities())
+                {
+                    model = db.NotesTab.Where(x => x.Id == model.Id).FirstOrDefault();
+                    txtTitle.Text = model.Title;
+                    txtNote.Text = model.Note;
+                }
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure to delete this record?", "Easy Note", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using(DBEntities db =new DBEntities())
+                {
+                    var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                        db.NotesTab.Attach(model);
+                    db.NotesTab.Remove(model);
+                    db.SaveChanges();
+                    PopulateDataGridView();
+                    Clear();
+                    MessageBox.Show("Deleted Successfully");
+                }
+            }
+        }
+    }
+}
